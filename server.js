04304586d4b1f5e6d68840e6bfc5386ec1c2a53c
Leong Ryan Chow <lcho0006@student.monash.edu>
2019-09-03 14:02:37 +1000

    Done

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 let express = require('express');
+let mongodb = require('mongodb');
+let MongoDBClient = mongodb.MongoClient;
 let app = express();
 let bodyParser= require('body-parser');
 
-let db = [];
 app.engine("html", require('ejs').renderFile);
 app.set('view engine','html');
 app.use(express.static('images'));
+app.use(express.static('css'));
 
 app.use(bodyParser.urlencoded({
     extended: false
 }))
+
+let db = null;
+let col = null;
+let url = "mongodb://localhost:27017"
+MongoDBClient.connect(url,{useNewUrlParser: true,useUnifiedTopology: true},function(err,client){
+    db = client.db('w6t1')
+    col=db.collection('Tasks');
+});
+
 
 app.get('/',function(req,res){
     res.sendFile(__dirname+"/index.html");
@@ -18,15 +29,52 @@ app.get('/',function(req,res){
 app.get('/addtask',function(req,res){
     res.sendFile(__dirname+"/addtask.html");
 });
+
+app.get('/deletetask',function(req,res){
+    res.sendFile(__dirname+"/deletetask.html");
+});
+
+app.get('/updatetask',function(req,res){
+    res.sendFile(__dirname+"/updatetask.html");
+});
+
+app.get('/deleteall',function(req,res){
+    res.sendFile(__dirname+"/deleteall.html");
+});
+
 app.get('/listtask',function(req,res){
-    res.render(__dirname+"/listtask.html", {
-        db: db
-    })
+    let query = {};
+    col.find(query).toArray(function(err,data){
+        res.render(__dirname+"/listtask.html",{ data: data });
+    });
+
 });
+
 app.post('/addtask',function(req,res){
-    db.push(req.body);
-    res.sendFile(__dirname+"/addtask.html");
+    let Obj = {TaskName: req.body.TaskName,
+        TaskAssign: req.body.TaskAssign,
+        TaskDue:req.body.TaskDue,
+        TaskStatus: req.body.TaskStatus,
+        TaskDesc: req.body.TaskDesc
+    };
+    col.insertOne(Obj);
+    res.redirect('/listtask');
 });
 
+app.post('/deletetask',function(req,res){
+    let query = {_id: new mongodb.ObjectID(req.body.TaskId)};
+    col.deleteOne(query);
+    res.redirect('/listtask');
+});
 
+app.post('/deleteall',function(req,res){
+    col.deleteMany({});
+    res.redirect('/listtask');
+});
+
+app.post('/updatetask',function(req,res){
+    col.updateOne({_id: new mongodb.ObjectID(req.body.TaskId)}, {$set:{TaskStatus:req.body.TaskStatus}},{upsert: false});
+    res.redirect('/listtask');
+});
+//sudo apt install -y mongodb
 app.listen(8080);
